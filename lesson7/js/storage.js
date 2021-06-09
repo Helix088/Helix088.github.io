cb82173d59414bf23a55939559d65ea6f82ac601
Helix088 <pat20001@byui.edu>
2021-06-09 18:44:36 -0500

    fixes to storage

--- lesson7/js/storage.js
+++ lesson7/js/storage.js
@@ -1,18 +1,16 @@
-localStorage.setItem("date1", new Date());
-localStorage.setItem("date2", new Date());
-
 function getNumberOfDays(start, end) {
-    let date1 = start;
-    let date2 = end;
-
     const oneDay = 1000 * 60 * 60 * 24;
 
-    const diffInTime =
-        localStorage.getItem("date2") - localStorage.getItem("date1");
+    const diffInTime = end - start;
 
     const diffInDays = Math.round(diffInTime / oneDay);
 
     return diffInDays;
 }
 
-console.log(getNumberOfDays("date1", "date2"));
+document.querySelector("span.visit").innerHTML = getNumberOfDays(
+    new Date(localStorage.getItem("date1")),
+    new Date()
+);
+
+localStorage.setItem("date1", new Date());
