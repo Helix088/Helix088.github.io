570b4e396c206e6ee6a51aa1ad647bb1ec2f0afd
Helix088 <pat20001@byui.edu>
2021-06-15 20:37:08 -0500

    fixes to prophets

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,16 +6,25 @@ fetch(requestURL)
         return response.json();
     })
     .then(function(jsonObject) {
+        const prophets = jsonObject["prophets"];
         console.table(jsonObject);
-        for (let i = 0; i < prophets.length; i++)
-            const prophets = jsonObject["prophets"];
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement("section");
+            let h2 = document.createElement("h2");
+            let bdate = document.createElement("p");
+            let bplace = document.createElement("p");
+            let image = document.createElement("img");
+
+            h2.textContent = prophets[i].name + " " + prophets[i].lastname;
+            bdate.textContent = prophets[i].birthdate;
+            bplace.textContent = prophets[i].birthplace;
+            image.setAttribute("src", prophets[i].imageurl);
+
+            card.appendChild(h2);
+            card.appendChild(bdate);
+            card.appendChild(bplace);
+            card.appendChild(image);
+
+            document.querySelector("div.cards").appendChild(card);
+        }
     });
-
-let card = document.createElement("section");
-let h2 = document.createElement("h2");
-
-h2.textContent = prophets[i].name + " " + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector("div.cards").appendChild(card);
