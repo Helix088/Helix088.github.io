ad6674f855cd9ac08c19cfd7ff53c4102712cfc3
Helix088 <pat20001@byui.edu>
2021-06-27 00:17:07 -0500

    fixes to five day forecast

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -3,39 +3,35 @@ const APPID = "f294865734f176c7193b898d22cbb23d";
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${APPID}&units=imperial`;
 
 fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        //console.log(jsObject);
-        const temperature = document.querySelector("#temperature");
-        temperature.textContent = jsObject.main.temp.toFixed(0);
-        const currently = document.querySelector("#currently");
-        const desc = jsObject.weather[0].description;
-        currently.innerHTML = `<strong>${desc.toUpperCase()}</strong>`;
-        const humidity = document.querySelector("#humidity");
-        humidity.textContent = jsObject.main.humidity.toFixed(0);
-        const speed = document.querySelector("#speed");
-        speed.textContent = jsObject.wind.speed;
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    const temperature = document.querySelector("#temperature");
+    temperature.textContent = jsObject.main.temp.toFixed(0);
+    const currently = document.querySelector("#currently");
+    const desc = jsObject.weather[0].description;
+    currently.innerHTML = `<strong>${desc.toUpperCase()}</strong>`;
+    const humidity = document.querySelector("#humidity");
+    humidity.textContent = jsObject.main.humidity.toFixed(0);
+    const speed = document.querySelector("#speed");
+    speed.textContent = jsObject.wind.speed;
 
-        let tempF = parseFloat(document.querySelector("#temperature").textContent);
-        let wind = parseFloat(document.querySelector("#speed").textContent);
+    let tempF = parseFloat(document.querySelector("#temperature").textContent);
+    let wind = parseFloat(document.querySelector("#speed").textContent);
 
-        if (tempF <= 50 && ((wind) => 3)) {
-            let wc = windChill(tempF, wind);
-            document.querySelector("#windchill").textContent = wc.toFixed(0);
-        } else {
-            document.querySelector("#windchill").textContent = "N/A";
-        }
+    if (tempF <= 50 && ((wind) => 3)) {
+      let wc = windChill(tempF, wind);
+      document.querySelector("#windchill").textContent = wc.toFixed(0);
+    } else {
+      document.querySelector("#windchill").textContent = "N/A";
+    }
 
-        function windChill(tempF, wind) {
-            let wc =
-                35.74 +
-                0.6215 * tempF -
-                35.75 * Math.pow(wind, 0.16) +
-                0.4275 * tempF * Math.pow(wind, 0.16);
-            return wc;
-        }
-
-        const imagesrc = `https://openweathermap.org/img/wn/${jsObject.weather[0].icon}.png`;
-        document.getElementById("icon").setAttribute("src", imagesrc);
-        document.getElementById("icon").setAttribute("alt", desc);
-    });
+    function windChill(tempF, wind) {
+      let wc =
+        35.74 +
+        0.6215 * tempF -
+        35.75 * Math.pow(wind, 0.16) +
+        0.4275 * tempF * Math.pow(wind, 0.16);
+      return wc;
+    }
+  });
--- lesson10/js/forecast2.js
+++ lesson10/js/forecast2.js
@@ -3,22 +3,25 @@ const appid = "f294865734f176c7193b898d22cbb23d";
 const apiURLL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${appid}&units=imperial`;
 
 fetch(apiURLL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        let day = 0;
-        const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-        //console.log(jsObject);
-        const fiveDayForecast = jsObject.list.filter((forecast) =>
-            forecast.dt_txt.includes("18:00:00")
-        );
-        //console.log(fiveDayForecast);
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let day = 0;
+    const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    //console.log(jsObject);
+    const fiveDayForecast = jsObject.list.filter((forecast) =>
+      forecast.dt_txt.includes("18:00:00")
+    );
+    //console.log(fiveDayForecast);
 
-        fiveDayForecast.forEach((x) => {
-            let d = new Date(x.dt_txt);
-            document.getElementById(`dayofweek${day + 1}`).textContent =
-                dayofWeek[d.getDay()];
-            document.getElementById(`forecast${day + 1}`).textContent =
-                x.main.temp.toFixed(0);
-            day++;
-        });
+    fiveDayForecast.forEach((x) => {
+      let d = new Date(x.dt_txt);
+      document.getElementById(`dayofweek${day + 1}`).textContent =
+        dayofWeek[d.getDay()];
+      document.getElementById(`forecast${day + 1}`).textContent =
+        x.main.temp.toFixed(0);
+      document.getElementById(
+        `imagesrc${day + 1}`
+      ).textContent = `https://openweathermap.org/img/wn/${x.weather[0].icon}.png`;
+      day++;
     });
+  });
