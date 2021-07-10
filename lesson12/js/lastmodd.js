088a97fdecc536dad6aae91c998fc8373e149694
Helix088 <pat20001@byui.edu>
2021-07-10 17:46:24 -0500

    initial commit

--- lesson12/js/lastmodd.js
+++ lesson12/js/lastmodd.js
@@ -1,9 +1,21 @@
-const options = {
-    year: "numeric",
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navagation");
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+window.onresize = () => {
+  if (window.innerWidth > 960) mainnav.classList.remove("responsive");
 };
-document.getElementById("theyear").textContent = new Date().toLocaleDateString(
-    "en-US",
-    options
+
+const datefield = document.querySelector("#lastmod");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
 );
 
-document.querySelector("#lastmod").textContent = document.lastModified;
+datefield.innerHTML = `<em>${fulldate}</em>`;
