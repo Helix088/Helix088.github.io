25ec1ab35951a89c05c8f9e1ffff885df40afbec
Chase Patterson <chase.patterson@uvu.edu>
2022-11-09 09:50:56 -0700

    fixed everything on site

--- lesson13/js/list-grid.js
+++ lesson13/js/list-grid.js
@@ -1,6 +1,6 @@
-const list = document.querySelector(".list");
-const grid = document.querySelector(".grid");
-const cards = document.querySelector(".cards");
+let list = document.querySelector(".list");
+let grid = document.querySelector(".grid");
+let cards = document.querySelector(".cards");
 
 list.addEventListener("click", () => {
     cards.classList.add("list");
